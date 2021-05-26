8328246387470336d234c74ffab2c85b7f7b1c2e
wuz <conlin@hackerrank.com>
2021-05-25 22:19:09 -0400

    Update login

--- src/Accounts/Login.js
+++ src/Accounts/Login.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { login } from "./auth";
 
+const text = (fn) => (e) => fn(e.target.value);
+
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onSubmit = () => {};
   return (
-    <form>
+    <form onSubmit>
       <label>
         Email
-        <input type="email" />
+        <input type="email" value={email} onChange={text(setEmail)} />
       </label>
       <label>
         Password
-        <input type="password" />
+        <input type="password" value={password} onChange={text(setPassword)} />
       </label>
       <button type="submit">Login</button>
     </form>
